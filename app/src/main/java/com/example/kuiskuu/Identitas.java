d9161801257a155341b256eed78610e747684c67
aqfa11 <80084765+aqfa11@users.noreply.github.com>
2021-07-03 16:44:57 +0700

    Initial commit

---
+++ app/src/main/java/com/example/kuiskuu/Identitas.java
@@ -0,0 +1,66 @@
+package com.example.kuiskuu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Identitas extends AppCompatActivity {
+    Button btnKerjakan;
+    EditText ednama, ednim;
+
+    String nama, nim;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_identitas);
+
+        btnKerjakan = findViewById(R.id.btnmasuk);
+        ednama = findViewById(R.id.isinama);
+        ednim = findViewById(R.id.isinim);
+
+        btnKerjakan.setOnClickListener(new View.OnClickListener() {
+            String nama, nim;
+
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(i);
+//                nama = "aqfa";
+//                nim = "090";
+//
+//                nama = ednama.getText().toString();
+//                nim = ednim.getText().toString();
+//
+//                if (nama.equals(nama) && nim.equals(nim)) {
+//                    Toast benar2 = Toast.makeText(getApplicationContext(),
+//                            "Login BERHASIL", Toast.LENGTH_LONG);
+//                    benar2.show();
+//                } else if (nama.equals(nama)) {
+//                    Toast namabenar = Toast.makeText(getApplicationContext(),
+//                            "nim SALAH", Toast.LENGTH_LONG);
+//                    namabenar.show();
+//                } else if (nim.equals(nim)) {
+//                    Toast nimbenar = Toast.makeText(getApplicationContext(),
+//                            "nama SALAH", Toast.LENGTH_LONG);
+//                    nimbenar.show();
+//                } else {
+//                    Toast semuasalah = Toast.makeText(getApplicationContext(),
+//                            "nama dan nim", Toast.LENGTH_LONG);
+//                    semuasalah.show();
+//                }
+
+//                Toast t =Toast.makeText(getApplicationContext(),
+//                        "E-Mail anda: " + nama +" dan Password anda: "+password,Toast.LENGTH_LONG);
+//                t.show();
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/com/example/kuiskuu/MainActivity.java
@@ -0,0 +1,97 @@
+package com.example.kuiskuu;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView kuis;
+    RadioGroup rg;
+    RadioButton PilihanA, PilihanB, PilihanC, PilihanD;
+    int nomor = 0;
+    public static int hasil, benar, salah;
+
+    //pertanyaan
+    String[] pertanyaan_kuis = new String[]{
+            "1. Ctrl+P adalah shortcut untuk memunculkan dialog....",
+            "2. Rukun Islam ada ?",
+            "3. Ibukota Indonesia adalah",
+            "4. Virus Corona muncul di Indonesia mulai tahun?",
+            "5. Bendera Negara Indonesia adalah"
+    };
+
+    //pilihan jawaban a, b, c, d
+    String[] pilihan_jawaban = new String[]{
+            "Print", "Customize", "Heater", "Page setup",
+            "2", "3", "4", "5",
+            "Jakarta", "Bogor", "Tangerang", "Bekasi",
+            "2021", "2020", "2019", "2018",
+            "Merah Biru Putih", "Merah Putih", "Putih Merah", "Belang-belang"
+    };
+
+    //jawaban benar
+    String[] jawaban_benar = new String[]{
+            "Print",
+            "5",
+            "Jakarta",
+            "2019",
+            "Merah Putih"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        kuis = (TextView) findViewById(R.id.kuis);
+        rg = (RadioGroup) findViewById(R.id.pilihan);
+        PilihanA = (RadioButton) findViewById(R.id.pilihanA);
+        PilihanB = (RadioButton) findViewById(R.id.pilihanB);
+        PilihanC = (RadioButton) findViewById(R.id.pilihanC);
+        PilihanD = (RadioButton) findViewById(R.id.pilihanD);
+
+        kuis.setText(pertanyaan_kuis[nomor]);
+        PilihanA.setText(pilihan_jawaban[0]);
+        PilihanB.setText(pilihan_jawaban[1]);
+        PilihanC.setText(pilihan_jawaban[2]);
+        PilihanD.setText(pilihan_jawaban[3]);
+
+        rg.check(0);
+        benar = 0;
+        salah = 0;
+    }
+
+    public void next(View view) {
+        if (PilihanA.isChecked() || PilihanB.isChecked() || PilihanC.isChecked() || PilihanD.isChecked()) {
+
+            RadioButton jawaban_user = (RadioButton) findViewById(rg.getCheckedRadioButtonId());
+            String ambil_jawaban_user = jawaban_user.getText().toString();
+            rg.check(0);
+            if (ambil_jawaban_user.equalsIgnoreCase(jawaban_benar[nomor])) benar++;
+            else salah++;
+            nomor++;
+            if (nomor < pertanyaan_kuis.length) {
+                kuis.setText(pertanyaan_kuis[nomor]);
+                PilihanA.setText(pilihan_jawaban[(nomor * 4) + 0]);
+                PilihanB.setText(pilihan_jawaban[(nomor * 4) + 1]);
+                PilihanC.setText(pilihan_jawaban[(nomor * 4) + 2]);
+                PilihanD.setText(pilihan_jawaban[(nomor * 4) + 3]);
+
+            } else {
+                hasil = benar * 20;
+                Intent selesai = new Intent(getApplicationContext(), com.example.kuiskuu.HasilKuis.class);
+                startActivity(selesai);
+            }
+        }
+        else {
+            Toast.makeText(this,"Kamu Jawab Dulu",Toast.LENGTH_LONG).show();
+        }
+    }
+}
